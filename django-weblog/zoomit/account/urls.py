80cb3057bcbf98fef2c23c817b2ef60974cf2a56
purya <puryabzp@gmail.com>
2020-12-21 10:54:21 +0330

    add new version of django-zoomit project

---
+++ django-weblog/zoomit/account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from account.views import Login, Logout, RegisterView
+
+urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
